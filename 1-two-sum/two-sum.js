fe0913cd1cb9edf58b5d411205173385f11d56e8
Hoàng Mạnh Thắng <21521428@gm.uit.edu.vn>
2024-06-21 22:37:14 +0700

    Time: 54 ms (86.31%) | Memory: 50.5 MB (51.59%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const numMap = {};
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (complement in numMap) {
+            return [numMap[complement], i]; 
+        }
+        numMap[nums[i]] = i;
+    }
+};
