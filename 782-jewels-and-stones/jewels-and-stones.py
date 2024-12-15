89bad9da6bc6fbdd12d518b1bf6fda9ad7ca7529
Hoàng Mạnh Thắng <21521428@gm.uit.edu.vn>
2024-12-15 16:28:36 +0700

    Time: 0 ms (100.00%) | Memory: 17.2 MB (13.24%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -1,7 +1,11 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         res = 0
+        jewel_map = {}
+        for jewel in jewels:
+            jewel_map[jewel] = True
+
         for stone in stones:
-            if stone in jewels:
+            if stone in jewel_map:
                 res += 1
         return res
